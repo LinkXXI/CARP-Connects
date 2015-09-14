2f68d394128e4aed45277f19cc2106601a9231b6
Kevin Lee <KevinRobertLee@gmail.com>
2015-09-14 19:32:07 -0400

    Added some more signup logic

--- client/lib/methods.js
+++ client/lib/methods.js
@@ -1,25 +1,40 @@
-singupUser = function(firstName, lastName, email, password, confirmPass, inviteCode){
+/**
+ * Client side methods
+ *
+ * This file contains Client Side only methods for re-use.
+ */
+
+singupUser = function (firstName, lastName, email, password, confirmPass, inviteCode) {
     /*
      This regex can be used to restrict passwords to a length of 8 to 20 aplhanumeric characters and select special characters. The password also can not start with a digit, underscore or special character and must contain at least one digit.
      ^(?=[^\d_].*?\d)\w(\w|[!@#$%]){7,20}
      */
 
-    if(password !== confirmPass){
+    if (password !== confirmPass) {
         return false;
     }
 
     var regex = new RegExp("^(?=[^\d_].*?\d)\w(\w|[!@#$%]){7,20}")
-    if(!regex.text(password)){
+    if (!regex.text(password)) {
         return false;
     }
 
-    if(!firstName || !lastName){
+    if (!firstName || !lastName) {
         return false;
     }
 
-    if(inviteCode){
-
-    }else{
-
+    Accounts.createUser({
+        email: email,
+        password: password,
+        profile:{
+            firstName: firstName,
+            lastName: lastName
+        }
+    });
+    //NOTE: New user is now logged in.
+    if (inviteCode) {
+        Meteor.call("validateInvitation", inviteCode)
     }
+
+    Router.go('/')
 };
