2d62394b90539125e5668f474c0bce2f402676ee
bkrissil <bkrissil@gmail.com>
2015-10-06 15:12:54 -0400

    account management complete

--- client/views/account/account_edit.js
+++ client/views/account/account_edit.js
@@ -24,12 +24,6 @@ Template.phone.helpers({
 });
 
 Template.accountEdit.events({
-    "click #passchange": function () {
-        $('#passchange-modal').openModal();
-    },
-    "click #passchange-cancel": function () {
-        $('#passchange-modal').closeModal();
-    },
     "click #add-email-cancel": function () {
         $('#add-email-modal').closeModal();
     },
@@ -170,35 +164,5 @@ Template.accountEdit.events({
                 $removeEmailModal.find('form')[0].reset();
             }
         });
-    },
-    "submit #passchange-form": function (e) {
-        e.preventDefault();
-        var password = {
-            current: $(e.target).find('#curpassword').val(),
-            new: $(e.target).find('#newpassword').val(),
-            confirmNew: $(e.target).find('#new2password').val()
-        };
-        //TODO: meteor add audit-argument-checks
-        /*
-         check(password, {
-         current: String,
-         new: String,
-         confirmNew: String
-         });
-         */
-        if (password.new === password.confirmNew) {
-            Accounts.changePassword(password.current, password.new, function (err) {
-                if (err) {
-                    throwError(err.reason);
-                } else {
-                    var $passchangeModal = $('#passchange-modal');
-                    $passchangeModal.closeModal();
-                    $passchangeModal.find('form')[0].reset();
-                }
-            });
-        } else {
-            throwError("Passwords don't match.");
-        }
-
     }
 });
--- client/views/account/account_view.js
+++ client/views/account/account_view.js
@@ -1,56 +1,39 @@
-Template.accountView.helpers({
-});
+Template.accountView.helpers({});
 
 Template.accountView.events({
-    "click #passchange": function(){
+    "click #passchange": function () {
         $('#passchange-modal').openModal();
     },
-    "click #passchange-cancel": function(){
+    "click #passchange-cancel": function () {
         $('#passchange-modal').closeModal();
-    },
-    "submit #acctmgmt-form": function (e) {
-        e.preventDefault();
-        var userFields = {
-                "profile.firstName": $(e.target).find('#firstname-input').val(),
-                "profile.lastName": $(e.target).find('#lastname-input').val(),
-                "profile.biography": $(e.target).find('#bio-input').val(),
-                "profile.skills": $(e.target).find('#skills-input').val()
-        };
-        Meteor.call('updateAccount', userFields, function (err) {
-            if(err){
-                console.log(err);
-                //throwError(err.reason);
-            } else {
-                Router.go('/');
-            }
-        });
     },
     "submit #passchange-form": function (e) {
         e.preventDefault();
         var password = {
-            current:$(e.target).find('#curpassword').val(),
+            current: $(e.target).find('#curpassword').val(),
             new: $(e.target).find('#newpassword').val(),
             confirmNew: $(e.target).find('#new2password').val()
         };
         //TODO: meteor add audit-argument-checks
         /*
-        check(password, {
-            current: String,
-            new: String,
-            confirmNew: String
-        });
-        */
+         check(password, {
+         current: String,
+         new: String,
+         confirmNew: String
+         });
+         */
         if (password.new === password.confirmNew) {
-            Accounts.changePassword(password.current, password.new, function(err){
-                if(err){
+            Accounts.changePassword(password.current, password.new, function (err) {
+                if (err) {
                     throwError(err.reason);
                 } else {
-                    $('#passchange-modal').closeModal();
+                    var $passchangeModal = $('#passchange-modal');
+                    $passchangeModal.closeModal();
+                    $passchangeModal.find('form')[0].reset();
                 }
             });
         } else {
             throwError("Passwords don't match.");
         }
-
     }
 });
