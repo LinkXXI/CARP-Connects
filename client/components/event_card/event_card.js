e8027e1eb02b54a8067c07c038d5b1a8317f0a8f
Benjamin Krissilas <bkrissil@gmail.com>
2015-10-08 13:38:20 -0400

    fixed google maps, reset to previous version if still broken

--- client/components/event_card/event_card.js
+++ client/components/event_card/event_card.js
@@ -1,14 +1,15 @@
 Template.eventCard.helpers({
     mapAttributes: function () {
+        var venue = venues.findOne({_id: this.venue});
         return {
             frameborder: "0",
             style: "border:0",
             src: "https://www.google.com/maps/embed/v1/place?q='" +
-            encodeURIComponent(this.venue.name.trim()) +
-            encodeURIComponent(this.venue.address.city.trim()) + ',' +
-            encodeURIComponent(this.venue.address.provinceState.trim()) + ',' +
-            encodeURIComponent(this.venue.address.postalZipCode.trim()) + ',' +
-            encodeURIComponent(this.venue.address.country.trim()) +
+            encodeURIComponent(venue.name.trim()) +
+            encodeURIComponent(venue.address.city.trim()) + ',' +
+            encodeURIComponent(venue.address.provinceState.trim()) + ',' +
+            encodeURIComponent(venue.address.postalZipCode.trim()) + ',' +
+            encodeURIComponent(venue.address.country.trim()) +
             "'&key=AIzaSyDez5ZF6K_UOr8izzd4PkXyWpAzG1dwTJQ"
         }
     }
