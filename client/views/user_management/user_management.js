329370eb930410fafc58cb360aa80f834fb452f0
Kevin Lee <KevinRobertLee@gmail.com>
2015-11-18 23:32:22 -0500

    permissions ui finished

--- client/views/user_management/user_management.js
+++ client/views/user_management/user_management.js
@@ -6,10 +6,10 @@ Template.userManagement.helpers({
 
 Template.user.events({
     "click #userOptions": function (e, template) {
-        $(this).find("#optionsModal").openModal();
+        $(template.find("#optionsModal")).openModal();
     },
     "click #userPermissions": function(e, template){
-        $(this).find("#permissionsModal").openModal();
+        $(template.find("#permissionsModal")).openModal();
     },
     "click #lockUser": function () {
         if (Meteor.userId() === this._id) {
@@ -44,6 +44,24 @@ Template.user.events({
                 })
             }
         })
+    },
+    "click #savePermissions": function(e, template){
+        var permissionsModal = $(template.find('#permissionsModal'));
+        var permissions = {};
+        permissions.role = permissionsModal.find('#roleSelect').val();
+        permissions.createEvent = permissionsModal.find('#hasCreateEvent')[0].checked;
+        permissions.editEvent = permissionsModal.find('#hasEditEvent')[0].checked;
+        permissions.publishEvent = permissionsModal.find('#hasPublishEvent')[0].checked;
+
+        Meteor.call("updatePermissions", this._id, permissions);
+    },
+    "click #cancelPermissions": function(e, template){
+        var permissionsModal = $(template.find('#permissionsModal'));
+        var permissions = this.profile.permissions;
+        permissionsModal.find('#roleSelect').val(permissions.role);
+        permissionsModal.find('#hasCreateEvent')[0].checked = permissions.createEvent;
+        permissionsModal.find('#hasEditEvent')[0].checked = permissions.editEvent;
+        permissionsModal.find('#hasPublishEvent')[0].checked = permissions.publishEvent;
     }
 });
 
@@ -56,5 +74,24 @@ Template.user.helpers({
     },
     profileComplete: function () {
         return Meteor.user().emails[0].verified && Meteor.user().profile.inviteCode && Meteor.user().profile.googleLinked;
+    },
+    hasPermission: function(permission){
+        switch(permission){
+            case EDT_EVENT:
+                return this.profile.permissions.editEvent;
+            case CREATE_EVENT:
+                return this.profile.permissions.createEvent;
+            case PUBLISH_EVENT:
+                return this.profile.permissions.publishEvent;
+            case CREATE_TASK:
+                return this.profile.permissions.createTask;
+            case EDIT_TASK:
+                return this.profile.permissions.editTask;
+            default:
+                return false;
+        }
+    },
+    isRole:function(role){
+        return role === this.profile.permissions.role;
     }
 });
