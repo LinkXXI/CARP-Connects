fd13a6c391a980587900c4ab5dfced0e8bd47cc7
Benjamin Krissilas <bkrissil@gmail.com>
2015-11-28 01:58:11 -0500

    my assigned tasks added to homepage, calendar now personalized to users (shows all tasks for unpublished events for all users, assigned tasks for unpublished events, and published events), legend added to calendar, calendar is now reactive (wasn't before)

--- client/views/calendar/calendar.js
+++ client/views/calendar/calendar.js
@@ -1,14 +1,28 @@
 /**
  * Created by Benjamin on 10/7/2015.
  */
-/*
- Tracker.autorun(function(){
- var eventsFetched = events.find().fetch();
- if (eventsFetched) {
- Session.set("eventsFetched", eventsFetched)
- }
- });
- */
+Template.calendar.onCreated(function () {
+    this.autorun(function () { // update calendar when changes made after initial load (this allows the calendar to be reactive)
+        var allEvents = events.find().fetch();
+        var calendar = $('#calendar');
+        calendar.fullCalendar('removeEvents');
+        calendar.fullCalendar('addEventSource', mapCalendarEvents(allEvents));
+    });
+});
+
+Template.calendar.onRendered(function () {
+    // add legend button to fullcalendar and initialize
+    $('.fc-right').prepend('<a href="#!" class="btn-flat dropdown-button no-margin-bot" data-activates="legend">Legend<i class="mdi-navigation-arrow-drop-down right"></i></a>');
+    $('.dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: true, // Displays dropdown below the button
+        alignment: 'left' // Displays dropdown with edge aligned to the left of button
+    });
+});
 
 function mapCalendarEvents(eventsArr) {
     var calendarEvents = [];
@@ -26,13 +40,15 @@ function mapCalendarEvents(eventsArr) {
             }
             if (eventTasks.length > 0) {
                 $.each(eventTasks, function (j, task) {
-                    calendarEvents.push({
-                        title: task.name,
-                        start: task.dateTime,
-                        url: Router.routes.TaskView.path({_id: task._id}),
-                        color: task.userIdAssignedTo == Meteor.userId() ? "#EF4444" : "#99CC99",
-                        allDay: false
-                    });
+                    if (event.status != "Complete") {
+                        calendarEvents.push({
+                            title: task.name,
+                            start: task.dateTime,
+                            url: Router.routes.TaskView.path({_id: task._id}),
+                            color: task.userIdAssignedTo == Meteor.userId() ? "#EF4444" : "#99CC99",
+                            allDay: false
+                        });
+                    }
                 });
             }
         });
--- client/views/index/index.js
+++ client/views/index/index.js
@@ -2,7 +2,36 @@
  * Created by darkl on 9/9/2015.
  */
 Template.index.helpers({
-    "calendarEvents": function() {
+    "calendarEvents": function () {
         return this.fetch();
+    },
+    assignedTasks: function () {
+        // search for incomplete events
+        var incompleteEvents = events.find({status: {$ne: "Complete"}}).fetch();
+        var incompleteEventsIds = [];
+        for (var i = 0; i < incompleteEvents.length; i++) {
+            incompleteEventsIds.push(incompleteEvents[i]._id);
+        }
+        // only get tasks for incomplete events
+        return tasks.find({
+            userIdAssignedTo: Meteor.userId(),
+            event: {$in: incompleteEventsIds}
+        }, {sort: {dateTime: 1}}).fetch();
+    },
+    getStatus: function () {
+        var color = "light blue";
+        if (this.status === "Not Started") {
+            color = "red lighten-4";
+        }
+        else if (this.status === "In Progress") {
+            color = "orange lighten-4";
+        }
+        else if (this.status === "Complete") {
+            color = "green lighten-4";
+        }
+        return color;
+    },
+    eventName: function (eventId) {
+        return events.findOne({_id: eventId}).name;
     }
 });
