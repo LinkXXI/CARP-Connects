5647e95224e6810b858bb982d265579b67ea86de
Sergio Parracho <parracho@sheridancollege.ca>
2015-12-02 19:25:05 -0500

    breadcrumbs

--- both/collections/enums.js
+++ both/collections/enums.js
@@ -2,5 +2,6 @@ Enumeration = {};
 Enumeration.eventStatus = [ "Active", "Complete" ];
 Enumeration.taskTypes = [ "Vendor", "Custom" ];
 Enumeration.taskStatus = [ "Not Started", "In Progress", "Complete" ];
-Enumeration.eventThemes = [ "Financial", "Health", "Retirement", "Senior Living", "Community", "Other" ];
 Enumeration.phoneType = [ "Home", "Cell", "Work", "Other" ];
+
+//Enumeration.eventThemes = [ "Financial", "Health", "Retirement", "Senior Living", "Community", "Other" ];
