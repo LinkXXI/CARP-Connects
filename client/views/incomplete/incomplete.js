a0d85051b35e6949216f302c5d2897edfa899433
Benjamin Krissilas <bkrissil@gmail.com>
2015-12-06 21:16:11 -0500

    further fixed google linked issue, admins do not lose admin access after linking to a new account, disallowed giving incomplete role to a user (assumption is that an incomplete role = incomplete profile), other roles (Admin, user) given are persistent even if given to a user with an incomplete profile

--- client/views/incomplete/incomplete.js
+++ client/views/incomplete/incomplete.js
@@ -1,51 +1,49 @@
 Template.incomplete.events({
     'click #google-button': function (e, template) {
         /*Meteor.loginWithGoogle({
-            requestPermissions: [""],
-            requestOfflineToken: true,
-            userEmail: Meteor.user().emails[0].address,
-            loginStyle: "popup"
-        }, function (error) {
-            if (error) {
-                console.log(error);
-            }
-        })*/
-        //TODO: CHECK IF GOOGLE ID IS USED ALREADY, WILL BREAK IF GOOGLE ID USED TWICE
+         requestPermissions: [""],
+         requestOfflineToken: true,
+         userEmail: Meteor.user().emails[0].address,
+         loginStyle: "popup"
+         }, function (error) {
+         if (error) {
+         console.log(error);
+         }
+         })*/
         Meteor.signInWithGoogle({
-            requestPermissions: ['email', 'https://www.googleapis.com/auth/drive.file'],
-            requestOfflineToken: true
-        },
+                requestPermissions: ['email', 'https://www.googleapis.com/auth/drive.file'],
+                requestOfflineToken: true
+            },
             function (err, mergedUserId) {
-                if(err){
+                if (err) {
                     console.log(err);
                     return;
                 }
-                console.log("User " + mergedUserId + " has been merged");
-
+                //console.log("User " + mergedUserId + " has been merged");
             }
         )
     },
-    'submit #invite-form': function(e, template){
+    'submit #invite-form': function (e, template) {
         e.preventDefault();
         var inviteCode = $(e.target).find("#invite-code").val();
-        Meteor.call("validateInvitation", inviteCode, function(err, data){
-            if(err || !data){
-                if(!data){
+        Meteor.call("validateInvitation", inviteCode, function (err, data) {
+            if (err || !data) {
+                if (!data) {
                     $(e.target).find("#invite-code-error").html("Invalid invitation code!");
-                }else if (err){
+                } else if (err) {
                     console.log(err);
                     $(e.target).find("#invite-code-error").html("An error has occurred!");
                 }
-            }else{
+            } else {
 
             }
         })
     },
-    'click #a-resend-verification': function(e, template){
-        Meteor.call('resendVerificationEmail', function(err, data){
-            if(err || !data){
+    'click #a-resend-verification': function (e, template) {
+        Meteor.call('resendVerificationEmail', function (err, data) {
+            if (err || !data) {
 
-            }else{
+            } else {
 
             }
         });
@@ -55,15 +53,23 @@ Template.incomplete.events({
 Template.incomplete.helpers({
     completed: function () {
         if (Meteor.user() && Meteor.user().emails) { // add emails check to fix error: Exception in template helper: TypeError: Cannot read property '0' of undefined
-            if ((Meteor.user().emails[0].verified && Meteor.user().profile.inviteCode && Meteor.user().profile.googleLinked)) {
-                Meteor.call('updatePermissions', null, {
-                    role:"user"
-                });
+            if (Meteor.user().emails[0].verified &&
+                Meteor.user().profile.inviteCode &&
+                Meteor.user().profile.googleLinked &&
+                Meteor.user().services &&
+                Meteor.user().services.google) { // do a sanity check for services.google as well, required to check if google is truly linked on the account
+                if (Meteor.user().profile.permissions.role != "Admin") { // avoid modifying Admin users
+                    Meteor.call('updatePermissions', Meteor.userId(), {
+                        role: "user"
+                    });
+                }
                 Router.go('/');
             } else { // make sure no role is given (this handles scenarios where googleLinked was changed to false for example)
-                Meteor.call('updatePermissions', null, {
-                    role:"incomplete"
-                });
+                if (Meteor.user().profile.permissions.role != "Admin") { // avoid modifying Admin users
+                    Meteor.call('updatePermissions', Meteor.userId(), {
+                        role: "incomplete"
+                    });
+                }
             }
         }
     },
@@ -85,6 +91,6 @@ Template.incomplete.helpers({
 
 });
 
-Template.incomplete.created = function(){
-  verifyEmail();
+Template.incomplete.created = function () {
+    verifyEmail();
 };
